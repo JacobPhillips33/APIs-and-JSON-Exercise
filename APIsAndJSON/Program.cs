e1d438901d6abc56af6bae82667cc77575a77681
Jacob Phillips <jakephillips.wv@gmail.com>
2022-10-11 17:25:16 -0500

    completed exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+
+            var ronVSKanye = new RonVSKanyeAPI(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {ronVSKanye.KayneQuote()}");
+                Console.WriteLine();
+                Thread.Sleep(500);
+                Console.WriteLine($"Ron: {ronVSKanye.RonQuote()}");
+                Console.WriteLine();
+                Thread.Sleep(500);
+            }
         }
     }
 }
