f16c14ddf17e3365fba9e876e01bcab3e54d448c
Jacob Phillips <jakephillips.wv@gmail.com>
2022-10-11 22:05:00 -0500

    completed exercise 2

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,53 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        
+        private readonly HttpClient _weatherClient;
+
+        public OpenWeatherMapAPI(HttpClient weatherClient)
+        {
+            _weatherClient = weatherClient;
+        }
+
+        public JToken CurrentTemp(string city_name) 
+        {
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=bd661ab51ec86047063cdc5b5851b037&units=imperial";
+
+            var weatherResult = _weatherClient.GetStringAsync(weatherURL).Result;
+
+            var jsonResult = JObject.Parse(weatherResult);
+
+            var tempFahrenheit = jsonResult["main"]["temp"];
+
+            return tempFahrenheit;
+        }
+
+        public JToken CurrentHumidity(string city_name)
+        {
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=bd661ab51ec86047063cdc5b5851b037&units=imperial";
+
+            var weatherResult = _weatherClient.GetStringAsync(weatherURL).Result;
+
+            var jsonResult = JObject.Parse(weatherResult);
+
+            var humidity = jsonResult["main"]["humidity"];
+
+            return humidity;
+        }
+
+        public JToken CurrentDescription(string city_name)
+        {
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=bd661ab51ec86047063cdc5b5851b037&units=imperial";
+
+            var weatherResult = _weatherClient.GetStringAsync(weatherURL).Result;
+
+            var jsonResult = JObject.Parse(weatherResult);
+
+            var description = jsonResult["weather"][0]["description"];
+
+            return description;
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Net.Http.Json;
 
 namespace APIsAndJSON
 {
@@ -7,8 +8,11 @@ namespace APIsAndJSON
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
+            #region Exercise 1: Ron vs Kayne
+            Console.WriteLine("***********************Exercise 1*****************************************");
+            Console.WriteLine();
 
+            var client = new HttpClient();
             var ronVSKanye = new RonVSKanyeAPI(client);
 
             for (int i = 0; i < 5; i++)
@@ -20,6 +24,35 @@ namespace APIsAndJSON
                 Console.WriteLine();
                 Thread.Sleep(500);
             }
+            #endregion Exercise 1: Ron vs Kayne
+
+            #region Exercise2: Current Weather By City
+            Console.WriteLine("***********************Exercise 2*****************************************");
+            Console.WriteLine();
+
+            var weatherClient = new HttpClient();
+            var weatherMap = new OpenWeatherMapAPI(weatherClient);
+
+            Console.WriteLine("This application returns weather information of a given city.");
+            Console.Write("Please enter a city: ");
+            var city = Console.ReadLine();
+            Console.WriteLine();
+
+            var temp = weatherMap.CurrentTemp(city);
+            var humidity = weatherMap.CurrentHumidity(city);
+            var description = weatherMap.CurrentDescription(city);
+            var cityUpper = city.ToUpper();
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"{cityUpper} WEATHER:");
+            Console.WriteLine();
+            Console.WriteLine($"Temperature (Fahrenheit): {temp} degrees");
+            Console.WriteLine($"Humidity:                 {humidity} percent");
+            Console.WriteLine($"Description:              {description}");
+            //Console.WriteLine($"The current temperature of {cityUpper} is {temp} degrees Fahrenheit.");
+                                    
+            #endregion Exercise2: Current Weather By City            
         }
     }
 }
